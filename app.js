c9ad287bde5fc9b8c7c37d9de552d69e9df9eb83
Kiran KS <kirankrish792@gmail.com>
2022-06-20 23:13:59 +0530

    Added home and monitor page

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ const express = require('express');
 const app = express();
 const path = require("path")
 const axios = require("axios").default
+const engine = require('ejs-mate');
 
 
-app.set('view engine',"ejs")
-app.set('views',path.join(__dirname,'views'));
+app.engine('ejs', engine);
 
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', "ejs")
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", async(req,res)=>{
-    const datas = await axios.get("https://io.adafruit.com/api/v2/pravi587/feeds/welcome-feed/data?X-AIO-Key=aio_NKPG68UIEgYhotbRZEmg6WBy2ZOL")
-    const values = datas.data
-    // console.log(values)
-    res.render("index",{values})
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get("/", async (req, res) => {
+    res.render('home')
+})
+
+app.get("/monitor", async (req, res) => {
+    const Temp = await axios.get("https://io.adafruit.com/api/v2/pravi587/feeds/temperature/data?X-AIO-Key=aio_NKPG68UIEgYhotbRZEmg6WBy2ZOL")
+    const Humidity = await axios.get("https://io.adafruit.com/api/v2/pravi587/feeds/humidity/data?X-AIO-Key=aio_NKPG68UIEgYhotbRZEmg6WBy2ZOL")
+    const tempValue = Temp.data
+    const humidityValue = Humidity.data
+    res.render("index", { tempValue, humidityValue })
 })
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('connected to port 3000');
 })
+
